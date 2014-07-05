bc2d6c04bd57046b719be97162915c8fb5299661
Arkadiy Drovosekov <arkdro@gmail.com>
2014-07-05 18:23:40 +0400

    move dir to outer level

---
+++ src/main/java/org/eff/Direction.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.eff;
+
+/**
+ *
+ * @author user1
+ */
+ enum Direction {
+    NW(-1, 1),  N(0, 1),  NE(1, 1),
+     W(-1, 0),             E(1, 0),
+    SW(-1, -1), S(0, -1), SE(1, -1);
+    private final int dx;
+    private final int dy;
+
+    public int getDx() {
+        return dx;
+    }
+
+    public int getDy() {
+        return dy;
+    }
+
+    Direction(int dx, int dy) {
+        this.dx = dx;
+        this.dy = dy;
+    }
+
+    static Direction flip_x_dir(Direction d) {
+        switch (d) {
+            case NW:
+                return NE;
+            case W:
+                return E;
+            case SW:
+                return SE;
+            case N:
+                return N;
+            case S:
+                return S;
+            case NE:
+                return NW;
+            case E:
+                return W;
+            case SE:
+                return SW;
+        }
+        assert false : "should not happen";
+        return NW;
+    }
+
+    static Direction flip_y_dir(Direction d) {
+        switch (d) {
+            case NW:
+                return SW;
+            case W:
+                return W;
+            case SW:
+                return NW;
+            case N:
+                return S;
+            case S:
+                return N;
+            case NE:
+                return SE;
+            case E:
+                return E;
+            case SE:
+                return NE;
+        }
+        assert false : "should not happen";
+        return NW;
+    }
+
+}
--- src/main/java/org/eff/Pig.java
+++ src/main/java/org/eff/Pig.java
@@ -15,7 +15,7 @@ public class Pig {
     private int x;
     private int y;
     private int speed, move_rest;
-    private dirs dir;
+    private Direction dir;
 
     /**
      * @return the x
@@ -38,63 +38,20 @@ public class Pig {
         return speed;
     }
 
-    private static enum dirs {
-
-        NW(-1, 1),  N(0, 1),  NE(1, 1),
-         W(-1, 0),             E(1, 0),
-        SW(-1, -1), S(0, -1), SE(1, -1);
-        private final int dx, dy;
-
-        dirs(int dx, int dy) {
-            this.dx = dx;
-            this.dy = dy;
-        }
-        private static dirs flip_x_dir(dirs d) {
-            switch(d) {
-                    case NW: return NE;
-                    case W: return E;
-                    case SW: return SE;
-                    case N: return N;
-                    case S: return S;
-                    case NE: return NW;
-                    case E: return W;
-                    case SE: return SW;
-            }
-            assert false : "should not happen";
-            return NW;
-        }
-
-        private static dirs flip_y_dir(dirs d) {
-            switch(d) {
-                    case NW: return SW;
-                    case W: return W;
-                    case SW: return NW;
-                    case N: return S;
-                    case S: return N;
-                    case NE: return SE;
-                    case E: return E;
-                    case SE: return NE;
-            }
-            assert false : "should not happen";
-            return NW;
-        }
-
-    }
-
     private static final int MAX_SPEED = 10;
     private static Random r = new Random();
     public void update_coordinates(int min_x, int max_x, int min_y, int max_y) {
-        int dx = dir.dx;
-        int dy = dir.dy;
+        int dx = dir.getDx();
+        int dy = dir.getDy();
         int new_x = getX() + dx;
         if(new_x < min_x || getX() > max_x) {
-            dir = dirs.flip_x_dir(dir);
+            dir = Direction.flip_x_dir(dir);
         } else {
             x = new_x;
         }
         int new_y = getY() + dy;
         if(new_y < min_y || getY() > max_y) {
-            dir = dirs.flip_y_dir(dir);
+            dir = Direction.flip_y_dir(dir);
         } else {
             y = new_y;
         }
@@ -105,8 +62,8 @@ public class Pig {
         this.y = y;
         speed = r.nextInt(MAX_SPEED) + 1;
         move_rest = MAX_SPEED;
-        int dir_idx = r.nextInt(dirs.values().length);
-        dir = dirs.values()[dir_idx];
+        int dir_idx = r.nextInt(Direction.values().length);
+        dir = Direction.values()[dir_idx];
     }
 
     @Override
