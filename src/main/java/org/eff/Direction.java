0492ab5a7e2b208b6bbcba5207e3a3492129ac06
Arkadiy Drovosekov <arkdro@gmail.com>
2014-07-14 16:28:25 +0400

    indentation

--- src/main/java/org/eff/Direction.java
+++ src/main/java/org/eff/Direction.java
@@ -10,7 +10,7 @@ package org.eff;
  *
  * @author user1
  */
- enum Direction {
+enum Direction {
     NW(-1, 1),  N(0, 1),  NE(1, 1),
      W(-1, 0),             E(1, 0),
     SW(-1, -1), S(0, -1), SE(1, -1);
