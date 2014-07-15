d66f2368c070ce55059628a7642cfdf2b13b3f0e
Arkadiy Drovosekov <arkdro@gmail.com>
2014-07-15 19:54:01 +0400

    plot pigs

--- src/main/java/org/eff/Place.java
+++ src/main/java/org/eff/Place.java
@@ -4,6 +4,8 @@
  */
 package org.eff;
 
+import javax.swing.*;
+import java.awt.*;
 import java.util.Random;
 
 /**
@@ -11,7 +13,8 @@ import java.util.Random;
  * @author user1
  */
 public class Place {
-
+    private int CELL_SIZE = 10;
+    private int EXTRA_SIZE = 50;
     private int width = 5;
     private int height = 5;
     private int n_pigs = 3;
@@ -40,6 +43,47 @@ public class Place {
         }
     }
 
+    private MyDrawPanel prepare_plot() {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        MyDrawPanel drawPanel = new MyDrawPanel();
+
+        frame.getContentPane().add(drawPanel);
+        frame.setSize(width * CELL_SIZE + EXTRA_SIZE, height * CELL_SIZE + EXTRA_SIZE);
+        frame.setVisible(true);
+        return drawPanel;
+    }
+
+    private Color choose_color(int i) {
+        Color[] colors = {
+            Color.BLACK,
+            Color.BLUE,
+            Color.CYAN,
+            Color.GREEN,
+            Color.MAGENTA,
+            Color.ORANGE,
+            Color.PINK,
+            Color.RED,
+            Color.YELLOW
+        };
+        int idx = (int) (Math.random() * colors.length);
+        return colors[idx];
+    }
+
+    private void plot_pigs(Graphics g) {
+        // g.fillOval(x, y, 40, 40);
+        // g.setColor(Color.green);
+        int idx = 0;
+        for (Pig pig : pigs) {
+            Color color = choose_color(idx++);
+            g.setColor(color);
+            int scaled_x = pig.getX() * CELL_SIZE;
+            int scaled_y = pig.getY() * CELL_SIZE;
+            g.fillRect(scaled_x, scaled_y, CELL_SIZE, CELL_SIZE);
+        }
+    }
+
     public Place() {
         init_pigsty();
     }
@@ -52,12 +96,23 @@ public class Place {
     }
 
     public void run() {
-        System.out.println("begin:");
-        print_pigsty();
-        for(int i = 0; i < 10; i++) {
+        MyDrawPanel drawPanel = prepare_plot();
+        for(int i = 0; i < 130; i++){
+//            System.out.println("i=" + i);
+//            print_pigsty();
             pigsty_one_step();
-            System.out.println("i=" + i);
-            print_pigsty();
+            drawPanel.repaint();
+            try {
+                Thread.sleep(100);
+            } catch (Exception ex) {}
+        }
+    }
+
+    class MyDrawPanel extends JPanel {
+        public void paintComponent(Graphics g){
+            g.setColor(Color.white);
+            g.fillRect(0,0,this.getWidth(),this.getHeight());
+            plot_pigs(g);
         }
     }
 }
