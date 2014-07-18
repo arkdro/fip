b82ae0ab04bc9b2dc44c54a5e483769bea84f767
Arkadiy Drovosekov <arkdro@gmail.com>
2014-07-18 17:44:47 +0400

    add wall class

---
+++ src/main/java/org/eff/Wall.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.eff;
+
+/**
+ *
+ * @author user1
+ */
+enum Wall {
+
+    SPACE,
+    CORNER,
+    HORIZONTAL_WALL,
+    VERTICAL_WALL
+}
