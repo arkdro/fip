f6830cf7d17f297a52eaa5034d521cfa76d46334
Arkadiy Drovosekov <arkdro@gmail.com>
2014-07-29 20:23:39 +0400

    make enum public

--- src/main/java/org/eff/Direction.java
+++ src/main/java/org/eff/Direction.java
@@ -11,7 +11,7 @@ import java.util.Random;
  *
  * @author user1
  */
-enum Direction {
+public enum Direction {
     NW(-1, 1),  N(0, 1),  NE(1, 1),
      W(-1, 0),             E(1, 0),
     SW(-1, -1), S(0, -1), SE(1, -1);
